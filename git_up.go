ca5ba69249fe593ca89ccddaa77e752288937c89
snowmerak <snowmerak@outlook.com>
2024-03-28 02:12:06 +0900

    change up to push

--- git_up.go
+++ git_up.go
@@ -7,7 +7,7 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-func up(ctx *cli.Context) error {
+func push(ctx *cli.Context) error {
 	branchName, err := getBranchName()
 	if err != nil {
 		return fmt.Errorf("failed to get current branch: %w", err)
@@ -17,20 +17,36 @@ func up(ctx *cli.Context) error {
 
 	switch len(sp) {
 	case 4:
+		if err := gitPush(); err != nil {
+			return fmt.Errorf("failed to push changes: %w", err)
+		}
+
 		if err := run("git", "switch", fmt.Sprintf("%s/%s", sp[0], sp[1])); err != nil {
 			return fmt.Errorf("failed to switch to feature branch: %w", err)
 		}
 	case 2:
+		if err := gitPush(); err != nil {
+			return fmt.Errorf("failed to push changes: %w", err)
+		}
+
 		if err := run("git", "switch", "develop"); err != nil {
 			return fmt.Errorf("failed to switch to develop branch: %w", err)
 		}
 	case 1:
 		switch sp[0] {
 		case "develop":
+			if err := gitPush(); err != nil {
+				return fmt.Errorf("failed to push changes: %w", err)
+			}
+
 			if err := run("git", "switch", "staging"); err != nil {
 				return fmt.Errorf("failed to switch to staging branch: %w", err)
 			}
 		case "staging":
+			if err := gitPush(); err != nil {
+				return fmt.Errorf("failed to push changes: %w", err)
+			}
+
 			if err := run("git", "switch", "prod"); err != nil {
 				return fmt.Errorf("failed to switch to prod branch: %w", err)
 			}
