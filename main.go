b3c3e35e2119e3b525bb60221193b6176abd1a5f
snowmerak <snowmerak@outlook.com>
2024-03-28 02:05:03 +0900

    make entrypoint

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := &cli.App{
+		Name:      "AX",
+		Usage:     "AX is a simple CLI tool for managing your project",
+		UsageText: "ax command [command options] [arguments...]",
+		Commands: []*cli.Command{
+			{
+				Name:      "init",
+				Aliases:   []string{"i"},
+				Usage:     "Initialize a new git repository",
+				UsageText: "ax init <remote-repository>",
+				Action:    initRepo,
+			}, {
+				Name:      "feature",
+				Aliases:   []string{"f"},
+				Usage:     "Create a new feature branch",
+				UsageText: "ax feature <feature-name>",
+				Action:    switchFeature,
+			}, {
+				Name:      "bugfix",
+				Aliases:   []string{"b"},
+				Usage:     "Create a new bugfix branch",
+				UsageText: "ax bugfix <bugfix-name>",
+				Action:    switchBugfix,
+			}, {
+				Name:      "hotfix",
+				Aliases:   []string{"x"},
+				Usage:     "Create a new hotfix branch",
+				UsageText: "ax hotfix <hotfix-name>",
+				Action:    switchHotfix,
+			}, {
+				Name:      "proposal",
+				Aliases:   []string{"p"},
+				Usage:     "Create a new proposal branch",
+				UsageText: "ax proposal <proposal-name>",
+				Action:    switchProposal,
+			}, {
+				Name:      "up",
+				Aliases:   []string{"u"},
+				Usage:     "Switch back to the previous branch",
+				UsageText: "ax back",
+				Action:    up,
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
