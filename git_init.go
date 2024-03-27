c2014825f5b9399f6bd05e0b7e6955b177e518c0
snowmerak <snowmerak@outlook.com>
2024-03-28 02:04:39 +0900

    make git init func

---
+++ git_init.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+func initRepo(ctx *cli.Context) error {
+	remoteRepogitory := ctx.Args().First()
+
+	if remoteRepogitory == "" {
+		return fmt.Errorf("error: remote repository is required")
+	}
+
+	if err := run("git", "init"); err != nil {
+		return fmt.Errorf("error initializing git repository: %w", err)
+	}
+
+	if err := run("git", "switch", "-C", "release"); err != nil {
+		return fmt.Errorf("error creating release branch: %w", err)
+	}
+
+	if err := os.WriteFile("README.md", []byte("# CHANGE ME!"), 0644); err != nil {
+		return fmt.Errorf("error creating README.md: %w", err)
+	}
+
+	if err := run("git", "add", "README.md"); err != nil {
+		return fmt.Errorf("error adding README.md: %w", err)
+	}
+
+	if err := run("git", "commit", "-m", "Initial commit"); err != nil {
+		return fmt.Errorf("error committing README.md: %w", err)
+	}
+
+	if err := run("git", "remote", "add", "origin", remoteRepogitory); err != nil {
+		return fmt.Errorf("error adding remote repository: %w", err)
+	}
+
+	if err := run("git", "push", "-u", "origin", "prod"); err != nil {
+		return fmt.Errorf("error pushing to remote repository: %w", err)
+	}
+
+	if err := run("git", "switch", "-C", "staging"); err != nil {
+		return fmt.Errorf("error creating develop branch: %w", err)
+	}
+
+	if err := run("git", "push", "-u", "origin", "staging"); err != nil {
+		return fmt.Errorf("error pushing to remote repository: %w", err)
+	}
+
+	if err := run("git", "switch", "-C", "develop"); err != nil {
+		return fmt.Errorf("error creating develop branch: %w", err)
+	}
+
+	if err := run("git", "push", "-u", "origin", "develop"); err != nil {
+		return fmt.Errorf("error pushing to remote repository: %w", err)
+	}
+
+	return nil
+}
